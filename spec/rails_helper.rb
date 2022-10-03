c841fe71deb430463d9bf4b848f625c6785c8fce
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-03 16:24:34 +0100

    Fix linter issues

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,11 +3,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require "capybara/rspec"
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -35,14 +35,14 @@ end
 
 Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
- end
- 
+end
+
 Capybara.javascript_driver = :selenium_chrome
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
-  config.infer_spec_type_from_file_location! 
+  config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
   config.before(:suite) do

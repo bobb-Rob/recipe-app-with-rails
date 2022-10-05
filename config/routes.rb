83a9cc50f54b0a438ca0de0c01e420f51bdabfaa
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-05 17:41:29 +0100

    Add custom path in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :recipes
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

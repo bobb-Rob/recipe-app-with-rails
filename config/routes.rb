c69f4b1941e67ca15393382781d95ba865ea460e
maen <maenkh@yahoo.com>
2022-10-05 17:26:52 +0300

    add nav bar , food list , add food form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index, :new, :create, :destroy ]
+  resources :users, only: [:index, :show, :new, :create, :destroy] 
+    
+
+  devise_for  :users,
+              :path => '',
+              :path_names => {  :sign_in =>       'login',
+                                :sign_out =>      'logout',
+                                :sign_up =>       '',
+                                :registration =>  'register',
+                                :edit =>          'edit',
+                                :cancel =>        'cancel',
+                                :confirmation =>  'verification'  }
+
 end

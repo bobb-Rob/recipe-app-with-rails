c9b6aa09acf9d26c594463e536bacb86962a914a
maen <maenkh@yahoo.com>
2022-10-07 01:48:28 +0300

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
-   resources :recipes, only: [:index, :new, :create, :destroy ]
-   resources :foods, only: [:index, :new, :create, :destroy ]
-   resources :users, only: [:index, :show, :new, :create, :destroy] 
+  
+ 
+  devise_for :users
+  root to: "home#index"
 
-  devise_scope :user do
-    root "devise/sessions#new"
-  end  
   
-  devise_for  :users,
-              :path => '',
-              :path_names => {  :sign_in =>       'login',
-                                :sign_out =>      'logout',
-                                :sign_up =>       '',
-                                :registration =>  'register',
-                                :edit =>          'edit',
-                                :cancel =>        'cancel',
-                                :confirmation =>  'verification'  }
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :recipes, only: [:index, :new, :create, :destroy ]
+  # resources :foods, only: [:index, :new, :create, :destroy ]
 
 end

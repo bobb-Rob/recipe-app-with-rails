0523911a4ea503fcb65c348abf7fb4f8d50ba3e6
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-06 11:11:15 +0100

    Add recipe details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  resources :recipes
+  resources :recipes  
 
   devise_for :users, skip: [:sessions]
   devise_scope :user do
     get 'signin', to: 'devise/sessions#new', as: :new_user_session
     post 'signin', to: 'devise/sessions#create', as: :user_session
-    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   

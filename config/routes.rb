121b77a7283ac4830f9a42c160584f65c0edac8e
maen <maenkh@yahoo.com>
2022-10-08 22:23:45 +0300

    finish linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :recipes, only: [:index, :new, :create, :destroy ]
+  resources :recipes, only: [:index, :new, :create, :destroy, :show ] do
+    resources :recipe_foods, only: [ :new, :create]
+  end
   resources :foods, only: [:index, :new, :create, :destroy ]
+  resources :shopping_lists, only: [:index ]
   
-  devise_for :users
 
   get 'public_recipes', to: 'recipes#public_recipes'
-  get 'shopping_list/index'  
-
+  
+  devise_for :users
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end

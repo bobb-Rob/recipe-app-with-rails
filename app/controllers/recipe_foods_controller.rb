ec362605b190a91d1a663faf27dfb8c8eabe7e51
maen <maenkh@yahoo.com>
2022-10-08 23:05:32 +0300

    fix linter

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,22 +1,19 @@
 class RecipeFoodsController < ApplicationController
-  
-  
   def new
-    @recipe_food = RecipeFood.new   
+    @recipe_food = RecipeFood.new
     @foods = current_user.foods
   end
 
-  def create    
+  def create
     @recipe_food = RecipeFood.new(recipe_id: params[:recipe_id],
                                   food_id: params[:food_id],
-                                  quantity: params[:quantity])    
-    @recipe_food.save        
-
+                                  quantity: params[:quantity])
+    @recipe_food.save
   end
 
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:recipe_id, :food_id,  :quantity)
+    params.require(:recipe_food).permit(:recipe_id, :food_id, :quantity)
   end
 end
--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,12 +1,9 @@
 class ShoppingListsController < ApplicationController
-    
-    def index    
-        @recipe_food = RecipeFood.joins(:food)
-        .select(:name,'recipe_foods.quantity - foods.quantity as q',:price)
-        .where('foods.quantity < recipe_foods.quantity')    
-    end
+  def index
+    @recipe_food = RecipeFood.joins(:food)
+      .select(:name, 'recipe_foods.quantity - foods.quantity as q', :price)
+      .where('foods.quantity < recipe_foods.quantity')
+  end
 
-    def new;
-    end
-
+  def new; end
 end
